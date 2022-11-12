b2d3caa42e0cc90a5003122f0e9707689c6b4824
Карташов Александр Евгеньевич <AKartashov2@alfabank.ru>
2022-11-12 20:19:52 +0300

    init service

---
+++ src/main/java/com/example/corelistenerapi/CoreListenerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.corelistenerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreListenerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreListenerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/corelistenerapi/CoreListenerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corelistenerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreListenerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
